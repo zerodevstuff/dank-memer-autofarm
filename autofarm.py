e6dc0f7593ce63651056140e6b62145acbc5ae96
zerodevstuff <68030052+zerodevstuff@users.noreply.github.com>
2020-09-06 11:13:37 -0400

    Create autofarm.py

---
+++ autofarm.py
@@ -0,0 +1,15 @@
+import pyautogui, time
+pyautogui.FAILSAFE = True
+time.sleep(5)
+print("made by Z-ERO#9181")
+count = 1
+while count < 999:
+    pyautogui.typewrite('pls search\n')
+    time.sleep(0.5)
+    pyautogui.typewrite('sewer\n')
+    time.sleep(0.5)
+    pyautogui.typewrite('pls beg\n')
+    time.sleep(1)
+    pyautogui.typewrite('pls use candy\n')
+    time.sleep(45)
+    count += 1 
